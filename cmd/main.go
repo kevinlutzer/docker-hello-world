cb427dbcd1bb8d83c69f84682a42f5e3238df3a4
Kevin Lutzer <kevinlutzer@proton.me>
2023-09-10 23:08:31 -0400

    change message to include a smiley emoji

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		w.Write([]byte("Hello, World!😀\n"))
 	})
 	http.ListenAndServe(":80", nil)
 }
